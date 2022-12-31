548674d4af9551b02e8feab08699d30254a3b9d0
NikolayRudakov <nrudakov@yandex.ru>
2022-12-31 14:13:58 +0300

    Sprint 19 first commit 31.12

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -39,7 +39,8 @@ ROBOTSTXT_OBEY = True
 
 # Override the default request headers:
 # DEFAULT_REQUEST_HEADERS = {
-#   'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#   'Accept': 'text/html,application/xhtml+xml,
+#       application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
 # }
 
